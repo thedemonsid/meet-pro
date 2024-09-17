2f9158d0b06e843d163f906880b36d5227f1577c
thedemonsid <siddheshshrirame@gmail.com>
2024-09-17 16:30:42 +0530

    chore : minor change

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} h-full`}>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar></Navbar>
         {children}
       </body>
